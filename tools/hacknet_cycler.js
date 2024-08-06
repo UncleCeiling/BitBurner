4b9df2bb1d3ab9e8f03ef9b2cad44a940ba15b42
UncleCeiling <topherf299@gmail.com>
2024-08-06 15:21:59 +0300

    Added more info to cycler

--- tools/hacknet_cycler.js
+++ tools/hacknet_cycler.js
@@ -1,13 +1,18 @@
 /** @param {NS} ns */
 export async function main(ns) {
+    ns.tprint('INFO - Starting Hacknet Cycler')
     let duration = ns.args[0] * 1000
     if (!(duration > 0)) { ns.tprint(`ERROR - "${duration}" is not a valid argument (please enter an integer larger than 1)`); return }
+    ns.tprint(`INFO - Duration: ${duration}`)
     let now = Date.now()
     let finish = now + duration
     while (Date.now() <= finish) {
         ns.run('queue/hacknet.js')
+        let estimate = Math.floor((finish - Date.now()) / 1000)
+        ns.tprint(`INFO - ${estimate} seconds until cycler ends`)
         while (ns.isRunning('queue/hacknet.js')) {
-            await ns.asleep(10)
+            await ns.asleep(1000)
         }
     }
+    ns.tprint(`SUCCESS - Hacknet Cycler complete`)
 }
