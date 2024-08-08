25a6c2ae57842619cd6447f21a02511e97032e7a
UncleCeiling <topherf299@gmail.com>
2024-08-08 22:46:08 +0300

    Wrote new hacknet (minus flag setting)

--- queue/hacknet.js
+++ queue/hacknet.js
@@ -1,120 +1,114 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    // Make functions for getting budget and nodeCost
+
+    // Get Budget
     function get_budget() { return ns.getServerMoneyAvailable('home') }
+
+    // Get node cost
     function get_node_cost() { return ns.hacknet.getPurchaseNodeCost() }
-    // If SQL exists, create the flag to stop spawning this scrip
-    let num_nodes = ns.hacknet.numNodes()
-    if (ns.fileExists('SQLInject.exe')) {
-        // Get node details
-        let node_details = []
-        for (let node = 0; node < num_nodes; node++) {
-            node_details.push(ns.hacknet.getNodeStats(node))
+
+    // Buy node or return `false`
+    function buy_node() {
+        let budget = get_budget()
+        let cost = get_node_cost()
+        if (budget > cost) {
+            ns.hacknet.purchaseNode()
+            history['nodes']++
+            history['spent'] += cost
+            return true
+        } else {
+            ns.print(`ERROR - Not enough cash to buy a node ($${cost.toLocaleString()})`)
+            return false
         }
-        // Check details 
-        let no_go = true
-        for (let node of node_details) {
-            if (node['level'] < 200 || node['ram'] < 64 || node['cores'] < 16) { no_go = false }
-        }
-        // If all nodes fully upgraded, make the flag
-        if (no_go) { ns.write('flags/hacknet.flag.txt', 'Hacknet halted'); return }
-    } else { ns.rm('flags/hacknet.flag.txt') }
-    // Get number of nodes
-    // Get the upgrade and history variables ready
-    let upgrade = { 'type': 'none', 'node': 0, 'cost': 0 }
-    let history = { 'ram': 0, 'cores': 0, 'levels': 0, 'nodes': 0, 'spent': 0, 'errors': 0 }
-    // Buy upgrades until we break
-    while (true) {
-        let level = { 'node': 0, 'cost': 0 }
-        let ram = { 'node': 0, 'cost': 0 }
-        let core = { 'node': 0, 'cost': 0 }
-        num_nodes = ns.hacknet.numNodes()
-        // Find the cheapest of each type of upgrade
-        if (num_nodes > 0) {
-            for (let node = 0; node < num_nodes; node++) {
-                // Get cost for this node
-                let levelCost = ns.hacknet.getLevelUpgradeCost(node, 1)
-                let ramCost = ns.hacknet.getRamUpgradeCost(node, 1)
-                let coreCost = ns.hacknet.getCoreUpgradeCost(node, 1)
-                // Compare to current best prices
-                if (levelCost > level["cost"] && levelCost < get_budget()) { level = { 'node': node, 'cost': levelCost } }
-                if (ramCost > ram["cost"] && ramCost < get_budget()) { ram = { 'node': node, 'cost': ramCost } }
-                if (coreCost > core["cost"] && coreCost < get_budget()) { core = { 'node': node, 'cost': coreCost } }
-            }
-            let nodeCost = get_node_cost()
-            if (ns.fileExists('SQLInject.exe') || nodeCost > get_budget()) { nodeCost = Infinity }
-            // Find the best upgrade
-            if (nodeCost != Infinity && nodeCost > Math.max(ram['cost'], core['cost'], ram['cost'])) {
-                upgrade = { 'type': 'node', 'node': 0, 'cost': nodeCost }
-            } else if (ram['cost'] > Math.max(core['cost'], level['cost'])) {
-                upgrade = { 'type': 'ram', 'node': ram['node'], 'cost': ram['cost'] }
-            } else if (core['cost'] > Math.max(ram['cost'], level['cost'])) {
-                upgrade = { 'type': 'core', 'node': core['node'], 'cost': core['cost'] }
-            } else if (level['cost'] > Math.max(ram['cost'], core['cost'])) {
-                upgrade = { 'type': 'level', 'node': level['node'], 'cost': level['cost'] }
+    }
+
+    // Buy level or return `false`
+    function buy_level(node) {
+        let budget = get_budget()
+        let levels = ns.hacknet.getNodeStats(node).level
+        let cost = ns.hacknet.getLevelUpgradeCost(node, 1)
+        if (levels == max_levels) { return false }
+        if (budget > cost) {
+            if (ns.hacknet.upgradeLevel(node, 1)) {
+                history['levels']++
+                history['spent'] += cost
+                return true
+            } else {
+                ns.print(`ERROR - Buying level on node ${node} failed. ($${cost.toLocaleString()})`)
+                return false
             }
         } else {
-            upgrade = { 'type': 'node', 'node': 0, 'cost': get_node_cost() }
+            ns.print(`ERROR - Not enough cash to buy level on node ${node}. ($${cost.toLocaleString()})`)
+            return false
         }
-        ns.print(upgrade)
-        // Buy the upgrade if we can afford it
-        if (get_budget() > upgrade['cost']) {
-            switch (upgrade['type']) {
-                // Buy a level
-                case 'level':
-                    if (ns.hacknet.upgradeLevel(upgrade['node'], 1)) {
-                        // Success! Add it to the history
-                        history['levels'] += 1
-                        history['spent'] = history['spent'] + upgrade['cost']
-                    } else {
-                        // Fail! add it to the errors
-                        history['errors'] += 1
-                        ns.print(`ERROR - Failed to upgrade Level on node ${upgrade['node']}`)
-                    } break;
-                // Buy some RAM
-                case 'ram':
-                    // Success! Add it to the history
-                    if (ns.hacknet.upgradeRam(upgrade['node'], 1)) {
-                        history['ram'] += 1
-                        history['spent'] = history['spent'] + upgrade['cost']
-                    } else {
-                        // Fail! add it to the errors
-                        history['errors'] += 1
-                        ns.print(`ERROR - Failed to upgrade Ram on node ${upgrade['node']}`)
-                    } break;
-                // Buy a core
-                case 'core':
-                    // Success! Add it to the history
-                    if (ns.hacknet.upgradeCore(upgrade['node'], 1)) {
-                        history['cores'] += 1
-                        history['spent'] = history['spent'] + upgrade['cost']
-                    } else {
-                        // Fail! add it to the errors
-                        history['errors'] += 1
-                        ns.print(`ERROR - Failed to upgrade Core on node ${upgrade['node']}`)
-                    } break;
-                // Buy a node
-                case 'node':
-                    if (ns.hacknet.purchaseNode() != -1) {
-                        history['nodes'] += 1
-                        history['spent'] = history['spent'] + upgrade['cost']
-                    } else {
-                        // Fail! add it to the errors
-                        history['errors'] += 1
-                        ns.print(`ERROR - Failed to buy node.`)
-                    }
-                    break;
-                // Fail! Add it to the tally
-                default:
-                    ns.print(`ERROR - Upgrade ${upgrade} failed`)
-                    history['errors'] += 1
-            }
-        }
-        // If we can't afford it, break
-        else { ns.print('ERROR - Ran out of funds'); break }
-        // Break if too many errors
-        if (history['errors'] >= 10) { ns.print('ERROR - Too many errors occurred'); break }
     }
+
+    // Buy ram or return `false`
+    function buy_ram(node) {
+        let budget = get_budget()
+        let ram = ns.hacknet.getNodeStats(node).ram
+        let cost = ns.hacknet.getRamUpgradeCost(node, 1)
+        if (ram == max_ram) { return false }
+        if (budget > cost) {
+            if (ns.hacknet.upgradeRam(node, 1)) {
+                history['ram']++
+                history['spent'] += cost
+                return true
+            } else {
+                ns.print(`ERROR - Buying RAM on node ${node} failed. ($${cost.toLocaleString()})`)
+                return false
+            }
+        } else {
+            ns.print(`ERROR - Not enough cash to buy RAM on node ${node}. ($${cost.toLocaleString()})`)
+            return false
+        }
+    }
+
+    // Buy core or return `false`
+    function buy_core(node) {
+        let budget = get_budget()
+        let cores = ns.hacknet.getNodeStats(node).cores
+        let cost = ns.hacknet.getCoreUpgradeCost(node, 1)
+        if (cores == max_cores) { return false }
+        if (budget > cost) {
+            if (ns.hacknet.upgradeCore(node, 1)) {
+                history['cores']++
+                history['spent'] += cost
+                return true
+            } else {
+                ns.print(`ERROR - Buying core on node ${node} failed. ($${cost.toLocaleString()})`)
+                return false
+            }
+        } else {
+            ns.print(`ERROR - Not enough cash to buy core on node ${node}. ($${cost.toLocaleString()})`)
+            return false
+        }
+    }
+
+    // Constants
+    let history = { 'nodes': 0, 'levels': 0, 'ram': 0, 'cores': 0, 'spent': 0 }
+    let max_levels = 200
+    let max_ram = 64
+    let max_cores = 16
+
+    // Get num of nodes
+    let num_nodes = ns.hacknet.numNodes()
+
+    // If no nodes, buy node
+    if (num_nodes <= 0) {
+        buy_node()
+        num_nodes = ns.hacknet.numNodes()
+    }
+
+    // For each node
+    for (let node = 0; node < num_nodes; node++) {
+        while (buy_ram(node)) { }
+        while (buy_core(node)) { }
+        while (buy_level(node)) { }
+    }
+
+    // If SQL Inject doesn't exist, buy nodes
+    while ((!ns.fileExists('SQLInject.exe')) && (get_budget() > get_node_cost())) { buy_node() }
 
     // Check the history and report what we spent
     if (history.spent > 0) {
