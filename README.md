6f1f5e687b6e6e345ba9db8184d14644d8662272
UncleCeiling <topherf299@gmail.com>
2024-08-11 23:38:12 +0300

    Started writing gang.js

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Structure
 
-### Running order
+### Running order (OUT OF DATE)
 
 ```md
 queue.js    | Calls the contents of "queue/" in sequence.
@@ -45,13 +45,55 @@ Augments
 
 Old versions of scripts
 
-### .servers
+## Planning
 
-A record of the things I've found in certain servers.
+### New Foreman
 
-### .vscode
+1. Read mines.txt
+2. Build queue of mining actions
+3. Build list of resources (most to least powerful)
+4. Take actions from queues and allocate to resources
+5. Run actions on 
 
-Configs for my VSCode workspace extensions:
+### Augment management
 
-- Bitburner extension
-- Spellcheck
+1. Create list of all available, un-purchased augments:
+    - Name
+    - Requires
+    - Factions
+2. Remove all entries that I don't have the requirements for.
+3. Check which augment costs the least reputation
+4. IF Check to see if we can buy it:
+    - Buy it and remove it from the list
+5. ELSE IF We don't have a gang:
+    - Work out what faction we're closest to
+    - Work out how to join faction
+    - Join faction
+    - Work for the faction until rep amount is met
+6. IF price multiplier > ?10?:
+    - Install owned augments
+
+### Contracts
+
+1. Build scripts for each contract
+2. Check all backdoored servers for contracts
+3. Deploy appropriate script
+
+### Gangs
+
+> The aim is to keep Wanted level at 0
+> Weakest 10% of gang, train
+> Next weakest, go gang things to raise money
+> Best members do Vigilant Justice
+
+Loop 1
+1. IF Karma > -54000:
+    - Farm kills
+2. Create Gang (Slum Snakes?)
+
+Loop 2
+1. Recruit Gang members
+2. Buy augments
+3. Ascend if necessary
+4. Buy equipment
+5. Assign jobs
---
+++ scripts/gang.js
@@ -0,0 +1,63 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    ns.disableLog('ALL')
+    const GANG_FACTION = "Slum Snakes" // My Chosen Faction
+    const EQUIPMENT = { // List of possible equipment
+        'WEAPONS': [
+            "Baseball Bat",
+            "Katana",
+            "Glock 18C",
+            "P90C",
+            "Steyr AUG",
+            "AK-47",
+            "M15A10 Assault Rifle",
+            "AWM Sniper Rifle"
+        ],
+        'ARMOR': [
+            "Bulletproof Vest",
+            "Full Body Armor",
+            "Liquid Body Armor",
+            "Graphene Plating Armor"
+        ],
+        'VEHICLES': [
+            "Ford Flex V20",
+            "ATX1070 Superbike",
+            "Mercedes-Benz S9001",
+            "White Ferrari"
+        ],
+        'ROOTKITS': [
+            "NUKE Rootkit",
+            "Soulstealer Rootkit",
+            "Demon Rootkit",
+            "Hmap Node",
+            "Jack the Ripper"
+        ],
+        'AUGMENTS': [
+            "Bionic Arms",
+            "Bionic Legs",
+            "Bionic Spine",
+            "BrachiBlades",
+            "Nanofiber Weave",
+            "Synthetic Heart",
+            "Synfibril Muscle",
+            "BitWire",
+            "Neuralstimulator",
+            "DataJack",
+            "Graphene Bone Lacings"
+        ]
+    }
+    const ENEMY_FACTIONS = [ // List of enemies
+        "Tetrads",
+        "The Syndicate",
+        "The Dark Army",
+        "Speakers for the Dead",
+        "NiteSec",
+        "The Black Hand"
+    ]
+    const ASCENSION_MULTIPLIER = 1.6487212707 // Minimum increase in stats for ascension
+    const MAX_MEMBERS = 12 // Max num of gang members
+    const TRAINING_PERCENT = 0.2 // Proportion of gang-members to 
+    const MIN_WIN_PERCENT = 0.8
+
+
+}
