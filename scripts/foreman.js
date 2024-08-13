9127de97e94713bb71ce260263439a4e77c64b07
UncleCeiling <topherf299@gmail.com>
2024-08-14 02:22:32 +0300

    Added smart timing to foreman and made sure it goes through the entire job queue before remaking the Queue

--- scripts/foreman.js
+++ scripts/foreman.js
@@ -6,7 +6,6 @@ export async function main(ns) {
     if (ns.getHostname() != HOST) { ns.tprint(`ERROR - Script must be run on 'home', not '${ns.getHostname()}'.`); return }
 
     while (true) {
-
         // Read mines.txt
         let mines = ns.read('mines.txt').split('\n')
 
@@ -25,21 +24,21 @@ export async function main(ns) {
                     'host': server.hostname,
                     'script': 'scripts/_weaken.js',
                     'threads': get_weaken_threads(server),
-                    // 'time': ns.getWeakenTime(server.hostname)
+                    'time': ns.getWeakenTime(server.hostname)
                 })
             } else if (server.moneyAvailable < server.moneyMax && server.moneyMax > 0 && server.moneyAvailable > 0) {
                 queue.push({
                     'host': server.hostname,
                     'script': 'scripts/_grow.js',
                     'threads': get_grow_threads(server),
-                    // 'time': ns.getGrowTime(server.hostname)
+                    'time': ns.getGrowTime(server.hostname)
                 })
             } else if (server.moneyAvailable == server.moneyMax && server.moneyMax > 0 && server.moneyAvailable > 0) {
                 queue.push({
                     'host': server.hostname,
                     'script': 'scripts/_hack.js',
                     'threads': get_hack_threads(server),
-                    // 'time': ns.getHackTime(server.hostname)
+                    'time': ns.getHackTime(server.hostname)
                 })
             } else { continue }
         }
@@ -58,37 +57,49 @@ export async function main(ns) {
         ns.print(`INFO - ${miners.size} Miners in pool`)
 
         // Take items off the list and give them to resources until they're full
-        while (miners.size > 0 && queue.length > 0) {
+        while (queue.length > 0) {
             // For resource in miners, take a job from the queue
             for (let miner of miners) {
                 // If queue is empty, move on
                 if (queue.length <= 0) { continue }
                 // If too many things running, skip this server
-                if (ns.ps(miner).length >= 4) { miners.delete(miner); continue }
+                if (ns.ps(miner).length >= 4) {
+                    ns.print(`WARN - Too many Scripts sleeping for ${DELAY * 1000 / 2} seconds.`)
+                    await ns.asleep(DELAY * 1000 / 2)
+                    continue
+                }
+
                 // Find free RAM on resource
                 let free_ram = ns.getServerMaxRam(miner) - ns.getServerUsedRam(miner)
                 // If running on HOST, leave 12 GB of RAM free
                 if (miner == HOST) { free_ram -= 12 }
+
                 // Pull the first job
                 let job = queue[0]
                 // Check that the resource has enough RAM to run the job, if not, remove it from the resource list
                 let job_ram = ns.getScriptRam(job.script, HOST)
-                if (free_ram < job_ram) { miners.delete(miner); continue }
+                if (free_ram < job_ram) {
+                    ns.print(`WARN - Not enough ram, sleeping for ${DELAY * 1000 / 2} seconds.`)
+                    await ns.asleep(DELAY * 1000 / 2)
+                    continue
+                }
+
                 // Find the Max Threads that the resource can do
                 let max_threads = Math.floor(free_ram / job_ram)
                 // If max_threads is lower than job threads, reduce job threads and run
                 if (max_threads < job.threads) {
                     queue[0].threads -= max_threads
                     run_job(job, miner, max_threads)
-                }
-                // Otherwise, remove the job from the list and run it
-                else {
+                } else { // Otherwise, remove the job from the list and run it
                     queue.shift()
                     run_job(job, miner, job.threads)
                 }
             }
+            if (queue.length > 0) {
+                ns.print(`WARN - ${queue.length} items in Queue, looping in ${DELAY} seconds.`)
+                await ns.asleep(DELAY)
+            }
         }
-        await ns.asleep(DELAY)
         // break
     }
 
