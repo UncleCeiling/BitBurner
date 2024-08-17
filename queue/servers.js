8b0e562b974c1838a157a7c81e3d83bed11ccd3b
UncleCeiling <topherf299@gmail.com>
2024-08-17 22:04:17 +0300

    Loosened requirement for skipping Server upgrade

--- queue/servers.js
+++ queue/servers.js
@@ -41,7 +41,7 @@ export async function main(ns) {
         let currentRam = ns.getServerMaxRam(server)
         let usedRam = ns.getServerUsedRam(server)
         // If not using more than half the ram AND too many things running, skip this server
-        if (usedRam <= currentRam / 2 && ns.ps(server).length >= 4) { continue }
+        if (usedRam <= currentRam / 2) { continue }
         // If at max RAM, skip this server
         if (currentRam >= ns.getPurchasedServerMaxRam()) { continue }
         // Find how much it will cost to get the next upgrade
