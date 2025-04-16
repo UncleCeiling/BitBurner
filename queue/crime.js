687bc8cb84e4803d381fdbd22a68f5a38783a90e
Chris Farfan <chrisfarfandelosgodos@gmail.com>
2025-04-17 02:46:27 +0300

    Moved crime to functional style Moved Sleeves to functional style and adjusted some behaviour

--- queue/crime.js
+++ queue/crime.js
@@ -5,84 +5,115 @@ export async function main(ns) {
     const CITIES = ["Aevum", "Chongqing", "Ishima", "New Tokyo", "Sector-12", "Volhaven"];
     let player_stats = ns.getPlayer();
     let karma = player_stats.karma;
-    // Farm Karma
-    if (karma > -54000) {
-        if (ns.singularity.getCurrentWork() == null) { do_homicide() } else if (ns.singularity.getCurrentWork().type != "CRIME") { do_homicide() } else if (ns.singularity.getCurrentWork().crimeType != "Homicide") {
-            do_homicide();
-            ns.tprint(`${ANSI.fg.cyan}Committing Homicide to decrease Karma${ANSI.reset}`);
-            return
-        } else { ns.tprint(`${ANSI.fg.cyan}Continuing to commit Homicide to decrease Karma${ANSI.reset}`); return };
-    };
-    // Join Bladeburners Division
-    if (
-        ns.bladeburner.inBladeburner() == false &&
-        player_stats.skills.strength >= 100 &&
-        player_stats.skills.defense >= 100 &&
-        player_stats.skills.dexterity >= 100 &&
-        player_stats.skills.agility >= 100
-    ) {
-        if (ns.bladeburner.joinBladeburnerDivision()) {
-            ns.tprint(`${ANSI.fg.cyan}Joined Bladeburner Division${ANSI.reset}`);
-        } else {
-            ns.tprint(`${ANSI.fg.red}Failed to join Bladeburner Division${ANSI.reset}`);
+    // Do the stuff
+    accept_invites();
+    join_bladeburners();
+    start_bladeburning();
+    if (farm_karma()) { return };
+    if (farm_kills()) { return };
+    work_for_factions();
+    // Search for more factions to get invites from?
+    next_city();
+
+    //#region ===== FUNCTIONS =====
+
+    /** Farm Karma */
+    function farm_karma() {
+        if (karma > -54000) {
+            if (ns.singularity.getCurrentWork() == null) { do_homicide() } else if (ns.singularity.getCurrentWork().type != "CRIME") { do_homicide() } else if (ns.singularity.getCurrentWork().crimeType != "Homicide") {
+                do_homicide();
+                ns.tprint(`${ANSI.fg.cyan}Committing Homicide to decrease Karma${ANSI.reset}`);
+                return true
+            } else { ns.tprint(`${ANSI.fg.cyan}Continuing to commit Homicide to decrease Karma${ANSI.reset}`); return true };
+        } else { return false }
+    }
+
+    /** Join Bladeburners*/
+    function join_bladeburners() {
+        // Join Division
+        if (
+            ns.bladeburner.inBladeburner() == false &&
+            player_stats.skills.strength >= 100 &&
+            player_stats.skills.defense >= 100 &&
+            player_stats.skills.dexterity >= 100 &&
+            player_stats.skills.agility >= 100
+        ) {
+            if (ns.bladeburner.joinBladeburnerDivision()) {
+                ns.tprint(`${ANSI.fg.cyan}Joined Bladeburner Division${ANSI.reset}`);
+            } else {
+                ns.tprint(`${ANSI.fg.red}Failed to join Bladeburner Division${ANSI.reset}`);
+            };
         };
-    };
+        // Join Faction
+        if (player_stats.factions.indexOf("Bladeburners") == -1 && ns.bladeburner.inBladeburner()) { if (ns.bladeburner.joinBladeburnerFaction()) { ns.tprint(`${ANSI.fg.cyan}Joined Bladeburner Faction${ANSI.reset}`) } };
+    }
 
-    // Join Bladeburners Faction
-
-    if (player_stats.factions.indexOf("Bladeburners") == -1 && ns.bladeburner.inBladeburner()) { if (ns.bladeburner.joinBladeburnerFaction()) { ns.tprint(`${ANSI.fg.cyan}Joined Bladeburner Faction${ANSI.reset}`) } };
-    // Start bladeburner script
-    if (!ns.isRunning("scripts/bladeburner.js")) {
-        if (ns.run("scripts/bladeburner.js") == 0) { ns.tprint(`${ANSI.fg.red}Failed to start bladeburner.js${ANSI.reset}`) }
-        else { ns.tprint(`${ANSI.fg.cyan}Started bladeburner.js${ANSI.reset}`) };
-    };
-    // Accept Faction invitations
-    let invitations = ns.singularity.checkFactionInvitations();
-    let installed_augments = ns.singularity.getOwnedAugmentations(true);
-    for (let invite of invitations) {
-        let invited_augments = ns.singularity.getAugmentationsFromFaction(invite);
-        invited_augments = invited_augments.filter((a) => !installed_augments.includes(a));
-        if (invited_augments.length > 0) {
-            if (ns.singularity.joinFaction(invite)) { ns.tprint(`${ANSI.fg.cyan}Joined ${invite} faction.${ANSI.reset}`) }
-            else { ns.tprint(`${ANSI.fg.red}Failed to join ${invite} faction.${ANSI.reset}`) }
+    /** Start bladeburner script */
+    function start_bladeburning() {
+        if (!ns.isRunning("scripts/bladeburner.js") && ns.bladeburner.inBladeburner()) {
+            if (ns.run("scripts/bladeburner.js") == 0) { ns.tprint(`${ANSI.fg.red}Failed to start bladeburner.js${ANSI.reset}`) }
+            else { ns.tprint(`${ANSI.fg.cyan}Started bladeburner.js${ANSI.reset}`) };
         };
-    };
-    // Find lowest faction rep augment and work for it
-    let joined_factions = ns.getPlayer().factions;
-    let faction_details = [];
-    for (let faction of joined_factions) {
-        let work = ns.singularity.getFactionWorkTypes(faction);
-        if (work.length == 0) { continue };
-        let faction_augments = ns.singularity.getAugmentationsFromFaction(faction).filter((a) => !installed_augments.includes(a));
-        let augments_to_buy = [];
-        for (let aug of faction_augments) { augments_to_buy.push({ "name": aug, "rep": ns.singularity.getAugmentationRepReq(aug) }) };
-        if (augments_to_buy <= 0) { continue };
-        faction_details.push({ "name": faction, "work": work, "augments_to_buy": augments_to_buy.sort((a, b) => b.rep - a.rep) });
-    };
-    if (faction_details.length > 0) {
-        faction_details = faction_details.sort((a, b) => a.augments_to_buy[0].rep - b.augments_to_buy[0].rep);
-        let faction_choice = faction_details[0];
-        let work_options = faction_choice.work;
-        let work_choice = "";
-        if (work_options.length == 1) { work_choice = work_options[0] };
-        if (work_options.length >= 2) { if (work_options.includes("field")) { work_choice = "field" } } else { work_choice = "security" };
-        if (ns.singularity.workForFaction(faction_choice.name, work_choice, false)) { ns.tprint(`${ANSI.fg.green}Doing ${work_choice} work for ${faction_choice.name}.${ANSI.reset}`) }
-        else { ns.tprint(`${ANSI.fg.red}Failed to start ${work_choice} work for ${faction_choice.name}.${ANSI.reset}`) };
-        return;
-    };
-    // Move City
-    let player_city = CITIES.indexOf(player_stats.city);
-    if (player_city <= 0) { player_city += CITIES.length };
-    let next_city = CITIES[player_city - 1];
-    ns.tprint(`${ANSI.fg.cyan}Travelling from ${player_stats.city} to ${next_city}.${ANSI.reset}`);
-    ns.singularity.travelToCity(next_city);
-    // Make sure enough people have been killed
-    if (player_stats.numPeopleKilled < 30) { do_homicide(); ns.tprint(`${ANSI.fg.cyan}Committing Homicide to increase body-count (${player_stats.numPeopleKilled}/30).${ANSI.reset}`) };
-    //!Search for more factions to get invites from? 
-    // ns.tprint(`${ANSI.fg.red}WIP${ANSI.reset}`);
+    }
 
-    // ===== FUNCTIONS =====
-    /** @param {NS} ns */
+    /** Accept Faction invitations */
+    function accept_invites() {
+        let invitations = ns.singularity.checkFactionInvitations();
+        let installed_augments = ns.singularity.getOwnedAugmentations(true);
+        for (let invite of invitations) {
+            let invited_augments = ns.singularity.getAugmentationsFromFaction(invite);
+            invited_augments = invited_augments.filter((a) => !installed_augments.includes(a));
+            if (invited_augments.length > 0) {
+                if (ns.singularity.joinFaction(invite)) { ns.tprint(`${ANSI.fg.cyan}Joined ${invite} faction.${ANSI.reset}`) }
+                else { ns.tprint(`${ANSI.fg.red}Failed to join ${invite} faction.${ANSI.reset}`) }
+            };
+        };
+    }
+
+    /** Find lowest faction rep augment and work for it */
+    function work_for_factions() {
+        let joined_factions = ns.getPlayer().factions;
+        let faction_details = [];
+        for (let faction of joined_factions) {
+            let work = ns.singularity.getFactionWorkTypes(faction);
+            if (work.length == 0) { continue };
+            let faction_augments = ns.singularity.getAugmentationsFromFaction(faction).filter((a) => !installed_augments.includes(a));
+            let augments_to_buy = [];
+            for (let aug of faction_augments) { augments_to_buy.push({ "name": aug, "rep": ns.singularity.getAugmentationRepReq(aug) }) };
+            if (augments_to_buy <= 0) { continue };
+            faction_details.push({ "name": faction, "work": work, "augments_to_buy": augments_to_buy.sort((a, b) => b.rep - a.rep) });
+        };
+        if (faction_details.length > 0) {
+            faction_details = faction_details.sort((a, b) => a.augments_to_buy[0].rep - b.augments_to_buy[0].rep);
+            let faction_choice = faction_details[0];
+            let work_options = faction_choice.work;
+            let work_choice = "";
+            if (work_options.length == 1) { work_choice = work_options[0] };
+            if (work_options.length >= 2) { if (work_options.includes("field")) { work_choice = "field" } } else { work_choice = "security" };
+            if (ns.singularity.workForFaction(faction_choice.name, work_choice, false)) { ns.tprint(`${ANSI.fg.green}Doing ${work_choice} work for ${faction_choice.name}.${ANSI.reset}`); return true }
+            else { ns.tprint(`${ANSI.fg.red}Failed to start ${work_choice} work for ${faction_choice.name}.${ANSI.reset}`) };
+            return true;
+        } else { return false }
+    }
+
+    /** Move City */
+    function next_city() {
+        let player_city = CITIES.indexOf(player_stats.city);
+        if (player_city <= 0) { player_city += CITIES.length };
+        let next_city = CITIES[player_city - 1];
+        ns.tprint(`${ANSI.fg.cyan}Travelling from ${player_stats.city} to ${next_city}.${ANSI.reset}`);
+        ns.singularity.travelToCity(next_city);
+    }
+
+    /** Make sure enough people have been killed */
+    function farm_kills() {
+        if (player_stats.numPeopleKilled < 30) { do_homicide(); ns.tprint(`${ANSI.fg.cyan}Committing Homicide to increase body-count (${player_stats.numPeopleKilled}/30).${ANSI.reset}`); return true } else { return false }
+    }
+
+    /** Do Homicide */
     function do_homicide() { ns.singularity.commitCrime("Homicide", false) };
+
+    //#endregion
+
 }
 
--- queue/sleeves.js
+++ queue/sleeves.js
@@ -4,22 +4,63 @@ import { ANSI } from "imports/ANSI";
 export async function main(ns) {
     ns.disableLog('ALL');
     // Initialise variables
-    let karma = (ns.heart.break() > -54000);
     let num_sleeves = ns.sleeve.getNumSleeves();
     // Do we have any sleeves?
-    if (num_sleeves <= 0) { buy_sleeves() };
+    if (num_sleeves <= 0) { return };
     // Build sleeve list
     let sleeves = [];
     for (let i = 1; i <= num_sleeves; i++) { sleeves.push(i) };
     // For each sleeve
     for (let sleeve in sleeves) {
-        // Farm Karma
-        if (karma) { ns.sleeve.setToCommitCrime(sleeve, "Homicide"); continue };
-        // Synchronise
-        if (ns.sleeve.getSleeve(sleeve).sync <= 99) { ns.sleeve.setToSynchronize(sleeve); continue };
-        // De-shock
-        if (ns.sleeve.getSleeve(sleeve).shock <= 99) { ns.sleeve.setToShockRecovery(sleeve); continue };
+        if (farm_karma(sleeve)) { continue }
+        if (synchronise_sleeve(sleeve)) { continue }
+        if (calm_sleeve(sleeve)) { continue }
     };
-    /** @param {NS} ns */
-    function buy_sleeves() { ns.tprint(`${ANSI.fg.red}No sleeves to manage.${ANSI.reset}`); return };
+
+    //#region ===== FUNCTIONS =====
+
+    /**
+     * Decides whether a sleeve should farm karma, then makes them if they should
+     * @param {Number} sleeve_num Number of the sleeve
+     * @returns {Boolean} `true` when karma farmed, `false` if not.
+     */
+    function farm_karma(sleeve_num) {
+        if (ns.heart.break() < -54000) {
+            let task = ns.sleeve.getTask(sleeve_num)
+            if (task == null || task.crimeType == null || task.crimeType != "Homicide") {
+                ns.sleeve.setToCommitCrime(sleeve, "Homicide"); return true
+            } else { return false }
+        }
+    }
+
+    /**
+     * Decides whether a sleeve should be synced or now, then makes them if they should
+     * @param {Number} sleeve_num Number of the sleeve
+     * @returns {Boolean} `true` when sleeve is being synced, `false` if not.
+     */
+    function synchronise_sleeve(sleeve_num) {
+        if (ns.sleeve.getSleeve(sleeve_num).sync <= 99) {
+            ns.sleeve.setToSynchronize(sleeve_num);
+            return true
+        } else { return false }
+    }
+
+    /**
+     * Decides whether a sleeve should be calmed or not, then makes them if they should
+     * @param {Number} sleeve_num Number of the sleeve
+     * @returns {Boolean} `true` when sleeve is being calmed, `false` if not.
+     */
+    function calm_sleeve(sleeve_num) {
+        if (ns.sleeve.getSleeve(sleeve_num).shock > 0) {
+            ns.sleeve.setToShockRecovery(sleeve_num);
+            return true
+        } else { return false }
+    }
+
+    //! WIP Need to know how to buy new sleeves and what the max number is
+    //! WIP Would also be good to work out sleeve augments
+
+    // function buy_sleeves() {
+    //     ns.tprint(`${ANSI.fg.red}No sleeves to manage.${ANSI.reset}`); return false
+    // };
 }
