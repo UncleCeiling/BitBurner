818b83f82241622f6eee272c6bbaa97200186dc0
Chris Farfan <chrisfarfandelosgodos@gmail.com>
2025-04-18 17:15:24 +0300

    Added crimes to sleeves Added crime success chance to sleeves

--- queue/sleeves.js
+++ queue/sleeves.js
@@ -12,9 +12,11 @@ export async function main(ns) {
     for (let i = 1; i <= num_sleeves; i++) { sleeves.push(i) };
     // For each sleeve
     for (let sleeve in sleeves) {
-        if (synchronise_sleeve(sleeve)) { continue }
-        if (farm_karma(sleeve)) { continue }
-        if (calm_sleeve(sleeve)) { continue }
+        if (farm_karma(sleeve)) { continue };
+        if (synchronise_sleeve(sleeve)) { continue };
+        if (calm_sleeve(sleeve)) { continue };
+        if (homicide(sleeve)) { continue };
+        heist(sleeve);
     };
 
     //#region ===== FUNCTIONS =====
@@ -55,6 +57,81 @@ export async function main(ns) {
             ns.sleeve.setToShockRecovery(sleeve_num);
             return true
         } else { return false }
+    }
+
+    /**
+     * Calculates the success chance for the sleeve attempting a crime.
+     * @param {Number} sleeve_num Number of the sleeve
+     * @returns {Number} Chance of success where 1 = 100%
+     */
+    function calc_crime_success(sleeve_num) {
+        let stats = ns.sleeve.getSleeve(sleeve_num);
+        const CRIME_WEIGHTS = {
+            "Homicide":
+            {
+                "hacking_weight": 0,
+                "strength_weight": 2,
+                "defense_weight": 2,
+                "dexterity_weight": 0.5,
+                "agility_weight": 0.5,
+                "charisma_weight": 0,
+                "difficulty": 1,
+            },
+            "Heist":
+            {
+                "hacking_weight": 1,
+                "strength_weight": 1,
+                "defense_weight": 1,
+                "dexterity_weight": 1,
+                "agility_weight": 1,
+                "charisma_weight": 1,
+                "difficulty": 18,
+            }
+        }
+        let crime_chances = {}
+        for (let crime of Object.keys(CRIME_WEIGHTS)) {
+            let chance = (
+                (CRIME_WEIGHTS[crime]["hacking_weight"] * stats.skills.hacking) +
+                (CRIME_WEIGHTS[crime]["strength_weight"] * stats.skills.strength) +
+                (CRIME_WEIGHTS[crime]["defense_weight"] * stats.skills.defense) +
+                (CRIME_WEIGHTS[crime]["dexterity_weight"] * stats.skills.dexterity) +
+                (CRIME_WEIGHTS[crime]["agility_weight"] * stats.skills.agility) +
+                (CRIME_WEIGHTS[crime]["charisma_weight"] * stats.skills.charisma) +
+                (0.025 * stats.skills.intelligence)
+            )
+            chance /= 975;
+            chance /= CRIME_WEIGHTS[crime]["difficulty"];
+            chance *= stats.mults.crime_success;
+            chance *= ns.getBitNodeMultipliers().CrimeSuccessRate;
+            chance *= 1 + (Math.pow(ns.getPlayer().skills.intelligence, 0.8)) / 600
+            crime_chances[crime] = chance
+        }
+        return Math.min(crime_chances, 1)
+    }
+
+    /**
+     * Decides if a sleeve should do homicide, then makes them if they should
+     * @param {Number} sleeve_num Number of the sleeve
+     * @returns {Boolean} `true` when sleeve is being calmed, `false` if not.
+     */
+    function homicide(sleeve_num) {
+        let current_task = ns.sleeve.getTask(sleeve_num)
+        if (current_task != null && current_task.crimeType != null && current_task.crimeType == "Homicide") { return true }
+        let success_chance = calc_crime_success(sleeve_num)["Homicide"];
+        if (success_chance < 1) { ns.sleeve.setToCommitCrime(sleeve_num, "Homicide"); return true };
+        return false
+    }
+
+    /**
+     * Decides if a sleeve should do homicide, then makes them if they should
+     * @param {Number} sleeve_num Number of the sleeve
+     * @returns {Boolean} `true` when sleeve is being calmed, `false` if not.
+     */
+    function heist(sleeve_num) {
+        let current_task = ns.sleeve.getTask(sleeve_num)
+        if (current_task != null && current_task.crimeType != null && current_task.crimeType == "Heist") { return true }
+        ns.sleeve.setToCommitCrime(sleeve_num, "Heist")
+        return true
     }
 
     //! WIP Need to know how to buy new sleeves and what the max number is
