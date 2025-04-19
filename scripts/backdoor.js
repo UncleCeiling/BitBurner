ac638960b41e1dba2bde012cd030200609699455
Chris Farfan <chrisfarfandelosgodos@gmail.com>
2025-04-19 21:07:35 +0300

    minor changes to backdoor

--- scripts/backdoor.js
+++ scripts/backdoor.js
@@ -12,12 +12,13 @@ export async function main(ns) {
     if (precursor.length >= 1) {
         ns.singularity.connect(precursor[0])
         ns.singularity.connect(target)
+        ns.clearLog()
         ns.ui.openTail()
-        ns.tprint(`SUCCESS - Starting backdoor on ${target}`)
+        ns.print(`SUCCESS - Starting backdoor on ${target}`)
         await ns.singularity.installBackdoor()
-        ns.tprint(`SUCCESS - Installed Backdoor on ${target}`)
         ns.ui.closeTail()
         ns.singularity.connect('home')
+        ns.tprint(`SUCCESS - Installed Backdoor on ${target}`)
     } else {
         ns.tprint(`WARN - Skipping Backdoor on ${target}. No adjacent backdoors:\n\t\t\t${ns.scan(target).join(' | ')}`)
     }
