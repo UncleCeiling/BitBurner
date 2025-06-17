158c1de59602000df9321c66de9dad9ce358ae3d
Chris Farfan <chrisfarfandelosgodos@gmail.com>
2025-06-17 18:01:47 +0300

    slowed nfg

--- shopping/player_augments.js
+++ shopping/player_augments.js
@@ -126,12 +126,11 @@ export async function main(ns) {
             };
         };
         if (augments.only_purchased.length > 0 && install) {
-            while (augments.neuroflux_governor.purchasable) {
+            if (augments.neuroflux_governor.purchasable) {
                 if (ns.singularity.purchaseAugmentation(augments.neuroflux_governor.factions[0], augments.neuroflux_governor.name)) {
                     ns.tprint(`${ANSI.fg.green}Bought ${augments.neuroflux_governor.name} from ${augments.neuroflux_governor.factions[0]}.${ANSI.reset}`);
                     install = false;
                 } else { ns.tprint(`${ANSI.fg.red}Error buying ${augments.neuroflux_governor.name} from ${augments.neuroflux_governor.factions[0]}.${ANSI.reset}`); };
-                await ns.asleep(100);
             };
         };
         if (augments.only_purchased.length > 0 && install) { ns.singularity.installAugmentations("boot.js"); };
