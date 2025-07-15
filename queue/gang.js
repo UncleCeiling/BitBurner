5795d0b200b060f56193b4ff55e076de48965caf
Chris Farfan de los Godos <chrisfarfandelosgodos@gmail.com>
2025-07-15 05:48:35 +0300

    implemented factors for mug scaling

--- queue/gang.js
+++ queue/gang.js
@@ -67,7 +67,9 @@ export async function main(ns) {
         if (member == "Dave 0" && wanted_level > 0 && gang_info.respect > 10) { ns.gang.setMemberTask(member, "Vigilante Justice") }; // If first dave and wanted is too high and respect is high enough, do justice
         if ((wanted_level > respect || wanted_rate > respect_rate) && gang_info.respect > 10) { ns.gang.setMemberTask(member, 'Vigilante Justice') } // If wanted is above respect (rate or level) and there is more than 10 respect
         else if (respect_gain != -1 && respect_gain <= 0) {
-            if ((Math.random() * (ns.gang.getMemberNames().length / 6) * ((10 * wanted_level) / respect) > 0.5)) { ns.gang.setMemberTask(member, "Train Combat") }
+            let member_factor = ns.gang.getMemberNames().length / 6 // doubles odds at full members, even odds at 6 members, 0.15x odds at 1 member.
+            let wanted_factor = respect / (10 * wanted_level) // If wanted penalty is 10%, even odds, if less than 10% then odds go up and vice versa.
+            if ((Math.random() * member_factor * wanted_factor) > 0.5) { ns.gang.setMemberTask(member, "Train Combat") }
             else { ns.gang.setMemberTask(member, "Mug People") }
         } // If we could calculate the respect gain and the respect gain is less than 0, Mug People
         else if (ns.gang.respectForNextRecruit() != Infinity && gang_info.respect < ns.gang.respectForNextRecruit()) { ns.gang.setMemberTask(member, "Terrorism") }
