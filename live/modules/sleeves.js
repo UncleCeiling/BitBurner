5afb990099115c5c851e247f9fee7bc602502d2a
Chris Farfan de los Godos <chrisfarfandelosgodos@gmail.com>
2025-09-12 04:39:16 +0300

    adjusted sleeve priority

--- live/modules/sleeves.js
+++ live/modules/sleeves.js
@@ -19,12 +19,12 @@ export async function main(ns) {
     if (ns.singularity.getCurrentWork()?.cyclesWorked === 0) {
       await ns.singularity.getCurrentWork().completion;
     }
-    if (calm_sleeve(sleeve, 95)) {
-      ns.print(`${ANSI.fg.cyan}${sleeve} Calming to 99% shock${ANSI.reset}`);
-      continue;
-    }
     if (farm_karma(sleeve)) {
       ns.print(`${ANSI.fg.cyan}${sleeve} Farming Karma${ANSI.reset}`);
+      continue;
+    }
+    if (calm_sleeve(sleeve, 50)) {
+      ns.print(`${ANSI.fg.cyan}${sleeve} Calming to 99% shock${ANSI.reset}`);
       continue;
     }
     if (farm_hacking(sleeve)) {
